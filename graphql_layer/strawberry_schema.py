b697dbf309c3a490f540f06de33164fa2c39e071
Clement <clement@leadiq.com>
2023-02-18 23:44:32 +0800

    add mutation to schema

--- graphql_layer/strawberry_schema.py
+++ graphql_layer/strawberry_schema.py
@@ -1,6 +1,7 @@
 from strawberry import Schema
 
+from graphql_layer.strawberry_mutation import Mutation
 from graphql_layer.strawberry_query import Query
 
 # Represents a GraphQL Schema; Contains the query and mutation endpoints available
-schema: Schema = Schema(query=Query)
+schema: Schema = Schema(query=Query, mutation=Mutation)
